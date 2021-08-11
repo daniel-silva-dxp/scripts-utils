e64f19e1d7b4276896800419696453edae08bb3a
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-11 16:20:44 -0300

    função para cformatar time milissegundos

--- src/convertDurationToTimeString.js
+++ src/convertDurationToTimeString.js
@@ -1,4 +1,4 @@
-export function convertDurationToTimeString(duration) {
+function convertDurationToTimeString(duration) {
   const hours = Math.floor(duration / 3600);
   const minutes = Math.floor((duration % 3600) / 60);
   const seconds = duration % 60;
