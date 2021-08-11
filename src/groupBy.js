b0be8dc0a3826414d74acb39bc677adbc68b88d5
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-11 16:16:28 -0300

    função para agrupar um objeto por propriedades

---
+++ src/groupBy.js
@@ -0,0 +1,11 @@
+function groupBy(arr, property) {
+  if (!arr) return;
+  return arr.reduce(function (acc, obj) {
+    var key = obj[property];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+}
