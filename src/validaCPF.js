f669ef8f579d3830c3ec60a05386dfb24fb80c8e
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-22 13:02:42 -0300

    refatoring: refatorando o código

--- src/validaCPF.js
+++ src/validaCPF.js
@@ -1,29 +1,29 @@
 const validaCpf = function (val) {
   function CPF() {
     function r(r) {
-      for (let t = null, n = 0; 9 > n; ++n)
+      for (var t = null, n = 0; 9 > n; ++n)
         t += r.toString().charAt(n) * (10 - n);
-      let i = t % 11;
+      var i = t % 11;
       return (i = 2 > i ? 0 : 11 - i);
     }
     function t(r) {
-      for (let t = null, n = 0; 10 > n; ++n)
+      for (var t = null, n = 0; 10 > n; ++n)
         t += r.toString().charAt(n) * (11 - n);
-      let i = t % 11;
+      var i = t % 11;
       return (i = 2 > i ? 0 : 11 - i);
     }
-    let n = false,
+    var n = false,
       i = true;
     (this.gera = function () {
-      for (let n = "", i = 0; 9 > i; ++i)
+      for (var n = "", i = 0; 9 > i; ++i)
         n += Math.floor(9 * Math.random()) + "";
-      let o = r(n),
+      var o = r(n),
         a = n + "-" + o + t(n + "" + o);
       return a;
     }),
       (this.valida = function (o) {
         for (
-          let a = o.replace(/\D/g, ""),
+          var a = o.replace(/\D/g, ""),
             u = a.substring(0, 9),
             f = a.substring(9, 11),
             v = 0;
@@ -32,7 +32,7 @@ const validaCpf = function (val) {
         )
           if ("" + u + f == "" + v + v + v + v + v + v + v + v + v + v + v)
             return n;
-        let c = r(u),
+        var c = r(u),
           e = t(u + "" + c);
         return f.toString() === c.toString() + e.toString() ? i : n;
       });
